6701c8110726a6eae968a81ecbafaac04755de8c
fqlCaua <13523450460@sina.cn>
2018-01-14 17:12:50 +0800

    新增

--- js/common.js
+++ js/common.js
@@ -1,19 +1,19 @@
-
-
 /*
  *iframe自适应高度
  *
  *
-*/
-  function reinitIframe(obj) {
-  var iframe = document.getElementById(obj);
-  
-  try {
-    var bHeight = iframe.contentWindow.document.body.scrollHeight;
-    var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+ */
+function reinitIframe(obj) {
+    var iframe = document.getElementById(obj);
 
-    var height = Math.max(bHeight, dHeight);
-    iframe.height = height;
-  } catch(e) {}
+    try {
+        var bHeight = iframe.contentWindow.document.body.scrollHeight;
+        var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+
+        var height = Math.max(bHeight, dHeight);
+        iframe.height = height;
+    } catch (e) {}
 }
 window.setInterval("reinitIframe('header')", 100);
+window.setInterval("reinitIframe('footer')", 100);
+window.setInterval("reinitIframe('main')", 100);
