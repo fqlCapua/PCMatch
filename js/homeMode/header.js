3ddb2aff6c0a3eeb8f82a2b3ebae8685735faf7b
WebArenaCN <13523450460@sina.cn>
2018-02-08 13:15:07 +0800

    新增

--- js/homeMode/header.js
+++ js/homeMode/header.js
@@ -1,7 +1,21 @@
-var Ajax = {
-    type: "POST",
-    url: "http://47.104.94.216/api/public/"
+var ReqUrl = "http://47.104.94.216/api/public/";
+
+function lock_string(str) {
+    var newLocker;
+    newLocker = hex_md5("my58_" + hex_md5("my58_" + str));
+    return newLocker;
 }
+function time_token() {
+    var timestamp,md_token,timeArr=[];
+    timestamp=Date.parse(new Date())/1000;
+    md_token=lock_string(timestamp);
+    timeArr.push(timestamp);
+    timeArr.push(md_token);
+    return timeArr;
+}
+
+
+
 
 function check_phone(Phone) {
 
@@ -55,14 +69,64 @@ function check_pwd(pwd) {
     return flag;
 }
 
+function Send_code() {
+    var Code;
+    var phone = $(".phone").val();
+    if (phone != "") {
+        var timeStamp = Date.parse(new Date) / 1000;
+        var md_token = hex_md5("my58_" + hex_md5("my58_" + timeStamp));
+        $.ajax({
+                url: 'http://47.104.94.216/api/public/',
+                type: 'POST',
+                async: false,
+                data: {
+                    service: "App.User.Get_code",
+                    time: timeStamp,
+                    token: md_token,
+                    user_phone: phone
+                },
+            })
+            .done(function(res) {
+                if (res.ret == 200) {
+                    Code = res.data.code;
+                } else {
+                    layer.msg(res.msg);
+                }
 
-function check_vCode(code) {
 
+            })
+            .fail(function(err) {
+
+            })
+            .always(function() {
+
+            });
+    } else {
+       layer.tips("不能为空", ".phone", {
+            tips: [1, "#4082D4"],
+            tipsMore: true
+        });
+    }
+
+    return Code;
+}
+
+function check_vCode() {
     var flag;
-    var reg = /^[a-zA-Z0-9]{6,18}$/;
 
     if ($(".vCode").val() != "") {
-      
+        var phoneCode = $(".vCode").val();
+        var phoneLocker = lock_string(phoneCode);
+        if (phoneLocker == Send_code()) {
+            flag = true;
+
+        } else {
+            layer.tips("验证码错误", ".vCode", {
+                tips: [1, "#4082D4"],
+                tipsMore: true
+            });
+            flag = false;
+        }
     } else {
         flag = false;
         layer.tips("不能为空", ".vCode", {
@@ -98,45 +162,13 @@ function check_PwdToPwd(pwd1, pwd2) {
     return flag;
 }
 
-function Send_code() {
-	var Code;
-    var timeStamp = Date.parse(new Date) / 1000;
-    var md_token = hex_md5("my58_" + hex_md5("my58_" + timeStamp));
-    $.ajax({
-            url: 'http://47.104.94.216/api/public/',
-            type: 'POST',
-            async:false,
-            data: {
-                service: "App.User.Get_code",
-                time: timeStamp,
-                token: md_token,
-                user_phone:"18882221111"
-            },
-        })
-        .done(function(res) {
-        	if(res.ret==200){
-        		Code=res.data.code;
-        	}else{
-        		layer.msg("发送失败");
-        	}
-            
-             
-        })
-       .fail(function(err) {
-          
-        })
-        .always(function() {
-             
-        });
-  return  Code;
-}
-console.log(Send_code())
+
+
 var InterValObj; //timer变量，控制时间 
 var count = 300; //间隔函数，1秒执行 
 var curCount; //当前剩余秒数
 function NoClick() {
     curCount = count;
-    Send_code(Phone)
     $(".provideBtn").attr("disabled", true);
     $(".provideBtn").val(curCount + "s后重新发送");
     timer1 = window.setInterval("remainTime()", 1000);
@@ -155,8 +187,31 @@ function remainTime() {
 
 }
 
-$(".logon-box a").click(function(){
-	parent.window.location.href="index.html#/login";
+function FormRegister(FormObj) {
+    $.ajax({
+            url: ReqUrl,
+            type: "POST",
+            data:FormObj,
+        })
+        .done(function(res) {
+           if(res.ret==200){
+           	 console.log(res.data);
+           }else{
+           	 layer.msg(res.msg)
+           }
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+
+
+}
+
+$(".logon-box a").click(function() {
+    parent.window.location.href = "index.html#/login";
 })
 
 $(".provideBtn").click(function() {
@@ -166,6 +221,23 @@ $(".provideBtn").click(function() {
         Send_code(phone);
     }
 });
+
 $(".submiBtn").click(function(event) {
-	//if(check_phone()&&check_pwd()&&)
+
+    var phone = $(".phone").val();
+    var pwd1 = $(".userpwd").val();
+    var pwd2 = $(".userpwd2").val();
+    var FormData={
+    	service:"App.User.Register",
+    	time:time_token()[0],
+    	token:time_token()[1],
+    	user_phone:phone,
+    	user_password:pwd2
+    }
+
+if (check_phone(phone) &&  check_vCode()&&check_pwd()  && check_PwdToPwd(pwd1.pwd2)) {
+          FormRegister(FormData);
+    }else{
+       
+    }
 });
