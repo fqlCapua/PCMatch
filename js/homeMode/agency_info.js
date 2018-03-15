50526f5fd56c6a0e7232c39265aa0b9d906d3eec
WebArenaCN <13523450460@sina.cn>
2018-03-15 17:10:15 +0800

    更新婚介所添加 删除（unfinished）

---
+++ js/homeMode/agency_info.js
@@ -0,0 +1,10 @@
+$(".agencyDetail").click(function(event) {
+	$(".agencyInfo").toggle();
+});
+$(".agencyCreate").click(function(event) {
+	$(".CreateNew").toggle();
+});
+$(".subNew").click(function(event) {
+	$(".CreateNew").toggle();
+});
+$("form").hide();
---
+++ js/jquery_image.js
@@ -0,0 +1,52 @@
+(function($) {
+    $.imageFileVisible = function(options) {
+        // 默认选项
+        var defaults = {
+            // 包裹图片的元素
+            wrapSelector : null,
+            // <input type=file />元素
+            fileSelector : null,
+            width : '100%',
+            height : 'auto',
+            errorMessage : "不是图片"
+        };
+        // Extend our default options with those provided.
+        var opts = $.extend(defaults, options);
+        $(opts.fileSelector).on("change", function() {
+           
+            var that=this;
+            $.each(this.files,function(index, el) {
+                
+           
+            var file = el;
+            if(typeof(file)){
+
+                $(opts.wrapSelector).html("");
+            }
+             var imgArr=[];
+            var imageType = /image.*/;
+            if (file.type.match(imageType)) {
+                   
+                  var reader = new FileReader();
+                    reader.onload = function() {
+                    var img = new Image();
+                    img.src = reader.result;//图片的base64位编码
+                    $(img).width(opts.width);
+                    $(img).height(opts.height);
+                   //imgArr.push(img)
+                   $(opts.wrapSelector).append(img);
+                    console.log(img)
+                  
+               };
+
+               reader.readAsDataURL(file);
+            } else {
+                $(opts.wrapSelector).html("选择的不是图片，将不会上传！");
+
+
+            }
+
+           });  
+        });
+    };
+})(jQuery);
